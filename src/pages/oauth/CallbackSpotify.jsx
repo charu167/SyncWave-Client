15509306f53bc0decb2e4608ee8d5ffdaff6b96a
Charudatta Potdar <charupot08@gmail.com>
2024-01-18 09:28:16 -0700

    catching code in callbackspotify route

--- src/pages/oauth/CallbackSpotify.jsx
+++ src/pages/oauth/CallbackSpotify.jsx
@@ -8,53 +8,60 @@ export default function CallbackSpotify() {
   const queryParams = new URLSearchParams(window.location.search);
 
   //Exchange spotify auth code for access & refresh tokens
-  useEffect(() => {
-    function getCode() {
-      // Set the local variable code to whatever auth code we recieve from spotify
-      if (queryParams.get("code") !== undefined) {
-        setCode(queryParams.get("code"));
-      }
-    }
+  // useEffect(() => {
+  //   function getCode() {
+  //     // Set the local variable code to whatever auth code we recieve from spotify
+  //     if (queryParams.get("code") !== undefined) {
+  //       setCode(queryParams.get("code"));
+  //     }
+  //   }
 
-    getCode();
-  }, [queryParams]);
+  //   getCode();
+  // }, [queryParams]);
 
-  async function exchangeCode() {
-    //Exchange code API call to backend
-    await axios
-      .post("/api/auth/spotify", {
-        code: code,
-      })
-      .then(async (res) => {
-        localStorage.setItem("spotifyAccessToken", res.data.spotifyAccessToken);
+  // async function exchangeCode() {
+  //   //Exchange code API call to backend
+  //   await axios
+  //     .post("/api/auth/spotify", {
+  //       code: code,
+  //     })
+  //     .then(async (res) => {
+  //       localStorage.setItem("spotifyAccessToken", res.data.spotifyAccessToken);
 
-        //Get spotify user details (user id)
-        await axios
-          .get("https://api.spotify.com/v1/me", {
-            headers: {
-              Authorization: "Bearer " + res.data.spotifyAccessToken,
-            },
-          })
-          .then((res1) => {
-            localStorage.setItem("spotifyID", res1.data.id);
-          });
+  //       //Get spotify user details (user id)
+  //       await axios
+  //         .get("https://api.spotify.com/v1/me", {
+  //           headers: {
+  //             Authorization: "Bearer " + res.data.spotifyAccessToken,
+  //           },
+  //         })
+  //         .then((res1) => {
+  //           localStorage.setItem("spotifyID", res1.data.id);
+  //         });
 
-        if (localStorage.getItem("spotifyAccessToken") !== undefined) {
-          navigate("/oauthgoogle");
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
+  //       if (localStorage.getItem("spotifyAccessToken") !== undefined) {
+  //         navigate("/oauthgoogle");
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }
 
   //
-  useEffect(() => {
-    setTimeout(() => {
-      exchangeCode();
-    }, 500);
-  });
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     exchangeCode();
+  //   }, 500);
+  // });
 
-  return <div></div>;
+  useEffect(() => {
+    setCode(queryParams.get("code"));
+  }, []);
+
+  return (
+    <div>
+      <h1>{code}</h1>
+    </div>
+  );
 }
